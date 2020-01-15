9158945c36468cb5768f884c9404ebd4ac46f48a
Kim <58223175+KimSieber@users.noreply.github.com>
2020-01-15 17:29:20 +0100

    Update day10.java

--- day10.java
+++ day10.java
@@ -7,7 +7,9 @@ public class Day10{
 		MS.runCheck();
 		int[] result = MS.getHighValue();
 		System.out.println("Hoechster Wert:"+result[2]+" an x/y="+result[0]+"|"+result[1]);
-		//MS.printMap();
+/** Testausgabe
+MS.printMap();
+**/
 		System.out.println("ready.");
     }
 }
@@ -23,18 +25,14 @@ class MonitoringStation{
 		String inputLine = this.reader.nextLine();
 		this.maxX = inputLine.length();
 		this.InputMap = new char[1][maxX];
-
 		this.InputMap[0] = inputLine.toCharArray(); 
-		
 		int i = 0;
 		while (reader.hasNextLine()){
 			i++;
 			inputLine = this.reader.nextLine();
-			
 			char[][] tmp = new char[i+1][maxX];
-            System.arraycopy(this.InputMap, 0, tmp, 0, i);
+            		System.arraycopy(this.InputMap, 0, tmp, 0, i);
 			this.InputMap = tmp;
-
 			this.InputMap[i] = inputLine.toCharArray();
 		}
 		this.maxY = i+1;
@@ -44,14 +42,13 @@ class MonitoringStation{
 	public void runCheck(){
 		for (int y=0; y<this.maxY; y++) {
 			for (int x=0; x<this.maxX; x++) {
-			    
 				if (this.InputMap[y][x] == '#') {
-					
 					this.ResultMap[y][x] = this.countVisible(x,y);
-					
+/** Testausgabe					
 if (x==11 && y==13) {
-//System.out.println("ResultMap["+y+"]["+x+"]="+ this.ResultMap[y][x]);
+	System.out.println("ResultMap["+y+"]["+x+"]="+ this.ResultMap[y][x]);
 }
+**/
 				}
 				
 			}
@@ -60,11 +57,9 @@ if (x==11 && y==13) {
 	
 	private int countVisible(int x1, int y1){
 		int count = 0;
-	
 		for (int y=0; y<this.maxY; y++) {
 			for (int x=0; x<this.maxX; x++) {
 				if (checkVisible(x1, y1, x, y)) {
-					
 					count++;
 				}
 			}
@@ -73,45 +68,47 @@ if (x==11 && y==13) {
 	}
 	
 	private boolean checkVisible(int x1, int y1, int x2, int y2) {
-		
 		// wenn sich selbst gefunden
 		if (x1==x2 && y1==y2) { return false; }
 		// wenn kein Asteroid
 		if (this.InputMap[y2][x2] != '#') { return false; }
-		
+		// Differenzen suchen
 		int DiffX = x2 - x1;
 		int DiffY = y2 - y1;
-		
+		// Minimale Differenz suchen, um "Linie" abzusuchen
 		int[] MinDiff = this.minimizeDiff(DiffX, DiffY);
 		int MinDiffX = MinDiff[0];
 		int MinDiffY = MinDiff[1];
-		
-		int tmpX, tmpY;
+		// Vorzeichen entfernen um höchsten Zähler für Schleife zu finden (Weg von xy1 zu xy2)
 		int MDX = (MinDiffX<0?MinDiffX*-1:MinDiffX);
 		int MDY = (MinDiffY<0?MinDiffY*-1:MinDiffY);
-
-if (x1==11 && y1==13 && y2==13) {
-    //System.out.println("checkVisible("+x1+","+y1+","+x2+","+y2+")  ->DiffX,Y="+DiffX+","+DiffY+"  MinDiffX,Y="+ MinDiffX+","+ MinDiffY);
-}
-
 		int iCnt = (MDX>MDY ? DiffX/MinDiffX : DiffY/MinDiffY);
 
-		
+/** Testausgabe
+if (x1==11 && y1==13 && y2==13) {
+	System.out.println("checkVisible("+x1+","+y1+","+x2+","+y2+")  ->DiffX,Y="+DiffX+","+DiffY+"  MinDiffX,Y="+ MinDiffX+","+ MinDiffY);
+}
+**/
+		// In Schleife Weg von xy1 zu xy2 abfahren und prüfen, wann erster Asteroid (=#) auftraucht
+		int tmpX, tmpY;	
 		for (int i=1; i<=iCnt; i++) {
 			tmpX = x1+(MinDiffX * i);
 			tmpY = y1+(MinDiffY * i);
+			// Wenn ein Asteroid gefunden ...
 			if (this.InputMap[tmpY][tmpX] == '#') {
+				// ... prüfen, ob es der geprüfte Asteroid (xy2) ist
 				if (x2 == tmpX && y2 == tmpY) {
-					
-					
-					if (x1==11 && y1==13 && y2==13) {
-//System.out.println("checkVisible("+x1+","+y1+","+x2+","+y2+")==true");
-//System.out.println("  ->iCnt="+iCnt+"  ->DiffX,Y="+DiffX+","+DiffY+"  MinDiffX,Y="+ MinDiffX+","+ MinDiffY);
-//System.out.println("  ->tmpX="+tmpX+",tmpY="+tmpY);
-					}
-				
+/** Testausgabe					
+if (x1==11 && y1==13 && y2==13) {
+	System.out.println("checkVisible("+x1+","+y1+","+x2+","+y2+")==true");
+	System.out.println("  ->iCnt="+iCnt+"  ->DiffX,Y="+DiffX+","+DiffY+"  MinDiffX,Y="+ MinDiffX+","+ MinDiffY);
+	System.out.println("  ->tmpX="+tmpX+",tmpY="+tmpY);
+}
+**/
+					// Wenn ja, dann ist xy2 in Sichtlinie zu xy1
 					return true;
 				} else {
+					// Wenn anderer Asteroid gefunden, dann ist xy2 verdeckt -> nicht in Sichtlinie
 					return false;
 				}
 			}
@@ -119,24 +116,24 @@ if (x1==11 && y1==13 && y2==13) {
 		return true;
 	}
 	
-	private int[] minimizeDiff(int DiffX, 
-	                           int DiffY) {
+	private int[] minimizeDiff(int DiffX, int DiffY) {
 		int[] result = {DiffX, DiffY};
+		// Bei 1 ist keine Division möglich, daher direkt Eigabe als MinimalDifferenz zurückgeben
 		if (DiffX== 1 || DiffY== 1 ||
 		    DiffX==-1 || DiffY==-1 ) {
 			return result;
 		}
+		// Divisionen prüfen, um Minimal-Differenz zu ermitteln
 		for (int i=20; i>0; i--) {
 			if (result[0] % i == 0  &&
 			    result[1] % i == 0 ) {
-				
-			    result[0] = result[0] / i;
+				result[0] = result[0] / i;
 				result[1] = result[1] / i;
 			}
 		}
-		
-//System.out.println("minimizeDiff("+ DiffX +","+ DiffY +") || result[]={"+ result[0] +","+ result[1]+"}");
-		
+/** Testausgabe		
+System.out.println("minimizeDiff("+ DiffX +","+ DiffY +") || result[]={"+ result[0] +","+ result[1]+"}");
+*/		
 		return result;
 	}
 	
@@ -156,13 +153,13 @@ if (x1==11 && y1==13 && y2==13) {
 	}
 	
 	public void printMap() {
-	    String tmp = " ";
+		String tmp = " ";
 		for (int y=0; y<this.maxY; y++) {
 			for (int x=0; x<this.maxX; x++) {
-	            tmp = tmp + this.InputMap[y][x];
-	        }
-	        System.out.println(tmp);
-	        tmp = " ";
-	    }
+	            		tmp = tmp + this.InputMap[y][x];
+	        	}
+	        	System.out.println(tmp);
+	        	tmp = " ";
+	    	}
 	}
 }
