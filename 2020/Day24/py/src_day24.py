82f1b4491c16aa2878e7bc665be109449954623b
Kim <58223175+KimSieber@users.noreply.github.com>
2020-12-28 12:50:09 +0100

    Update src_day24.py

--- 2020/Day24/py/src_day24.py
+++ 2020/Day24/py/src_day24.py
@@ -33,8 +33,7 @@ def readInput(file_name):
     rem         = ""
     for line in lines:
         orders      = []
-        letters = list(line)
-        for letter in letters:
+        for letter in list(line):
             if letter in ("s", "n"):
                 rem = letter
             else:
@@ -55,9 +54,9 @@ def readInput(file_name):
 def flipTile(insts, grid):
     x = y = 0
     for inst in insts:
-        if inst == "e":
+        if inst == "e":             
             x += 2
-        elif inst == "w":
+        elif inst == "w":           
             x -= 2
         elif inst == "se":
             x += 1
@@ -71,13 +70,10 @@ def flipTile(insts, grid):
         elif inst == "nw":
             x -= 1
             y -= 1
-    if (y,x) not in grid.keys():
-        grid[(y,x)] = 1
+    if (y,x) not in grid.keys():            grid[(y,x)] = 1
     else:
-        if grid[(y,x)] == 0:
-            grid[(y,x)] = 1
-        else:
-            grid[(y,x)] = 0
+        if grid[(y,x)] == 0:                grid[(y,x)] = 1
+        else:                               grid[(y,x)] = 0
     return grid
 
 ### Ermittelt die Anzahl der schwarzen Fliesen auf dem Grid
@@ -85,8 +81,8 @@ def flipTile(insts, grid):
 ### @return         : (int) Anzahl schwarze Fliesen
 def cntBlackTiles(grid):
     cnt = 0
-    for key in grid:
-        if grid[key] == 1:     cnt += 1
+    for key in (key for key in grid if grid[key] == 1): 
+        cnt += 1
     return cnt
 
 
@@ -94,7 +90,6 @@ tile_list = readInput(FILENAME)
 
 for tile_insts in tile_list:
     grid = flipTile(tile_insts, grid)
-
 
 print()
 print("PART I  : Number of tiles with the black side up : ", cntBlackTiles(grid))
@@ -105,16 +100,8 @@ print()
 ### @pos            : (tuple) (y, x) - Koordinaten der Ausgangsfliese
 ### @return[0..5]   : (tuple) (y, x) - Koordinaten der 6 umliegenden Fliesen
 def getTilesAdj(pos):
-    ret = []
-    y = pos[0]
-    x = pos[1]
-    ret.append((y  ,x+2))       #=> e
-    ret.append((y  ,x-2))       #=> w
-    ret.append((y+1,x+1))       #=> se
-    ret.append((y+1,x-1))       #=> sw
-    ret.append((y-1,x+1))       #=> ne
-    ret.append((y-1,x-1))       #=> nw
-    return ret
+    return [(pos[0]  ,pos[1]+2), (pos[0]  ,pos[1]-2), (pos[0]+1,pos[1]+1), \
+            (pos[0]+1,pos[1]-1), (pos[0]-1,pos[1]+1), (pos[0]-1,pos[1]-1)]
 
 ### Ermittelt die Anzahl der angrenzenden Fliesen, die schwarz sind
 ### @grid[(y,x)]    : (int) Fliesenzustand an Koordinate Y,X
@@ -123,66 +110,47 @@ def getTilesAdj(pos):
 def getNumBlackTilesAdj(grid, pos):
     cnt_black = 0
     tiles_adj = getTilesAdj(pos)
-    for tile in tiles_adj:
-        if tile in grid.keys():
-            if grid[tile] == 1:     
-                cnt_black += 1
+    for tile in (xy for xy in tiles_adj if xy in grid.keys()):
+        if grid[tile] == 1:        cnt_black += 1
     return cnt_black
 
 ### Prueft Fliese und gibt (neue) Farbe zurueck
 ### @grid[(y,x)]    : (int) Fliesenzustand an Koordinate Y,X
 ### @pos            : (tuple) (y, x) - Koordinaten der Ausgangsfliese
 def checkTileNewColor(grid, pos):
-    cntBlackTiles = getNumBlackTilesAdj(grid,pos)
     if pos in grid.keys():          color = grid[pos]
     else:                           color = 0
-    ret = color
-    if color == 1 and (cntBlackTiles == 0 or cntBlackTiles > 2):
-        ret = 0
-    elif color == 0 and cntBlackTiles == 2:
-        ret = 1
-    return ret
+    cntBlackTiles = getNumBlackTilesAdj(grid,pos)
+    if   color == 1 and (cntBlackTiles == 0 or cntBlackTiles > 2):    return 0
+    elif color == 0 and  cntBlackTiles == 2:                          return 1
+    else:                                                             return color
 
 
-### Durchlaeft alle Fliesen und bei Schwarz alle Nachbarfliesen und
+### Durchlaeuft alle vorh. Fliesen und alle Nachbarfliesen und
 ### fruehrt Aenderungspruefung aus.
-### Aenderungsbedarf wird gespeichert und anschliessend gesamthaft ausgefuert
+### Aenderungsbedarf wird gespeichert und anschliessend gesamthaft ausgefuehrt
 ### @grid[(y,x)]            = (int) Fliesenzustand an Koordinate Y,X
 ### @return     grid[(y,x)] = (int) Fliesenfarbe - aktualisiertes grid
 def runOneDay(grid):
     chg_log = {}
-
-    def getTileColor(pos):
-        if pos in grid.keys():  return grid[pos]
-        else:                   return 0
-
     ### Alle gespeicherten Tiles pruefen
     for tile in grid.keys():
-        color     = grid[tile]
         new_color = checkTileNewColor(grid,tile)
-        if color != new_color:
-            chg_log[tile] = new_color
-        ### von jeder umliegenden Fliese Zustand pruefen, wenn nicht in grid
+        if new_color != grid[tile]:  chg_log[tile] = new_color
+        ### von jeder umliegenden Fliese Zustand pruefen, wenn nicht in grid (sonst schon oben geprueft)
         tiles_adj = getTilesAdj(tile)
-        for tile_adj in tiles_adj:
-            if tile_adj not in grid.keys():
-                color_adj     = getTileColor(tile_adj)
-                new_color_adj = checkTileNewColor(grid,tile_adj)
-                if color_adj != new_color_adj:
-                    chg_log[tile_adj] = new_color_adj
-
-    ### Alle Aenderungen ausfuehren => weisse Fliesen loeschen
+        for tile_adj in (xy for xy in tiles_adj if xy not in grid.keys()):
+            if checkTileNewColor(grid,tile_adj) == 1:   chg_log[tile_adj] = 1
+    ### Alle Aenderungen ausfuehren (wenn Fliese nicht in grid und neue Farbe weiss, dann nicht erzeugen)
     for key in chg_log:
-        if chg_log[key] == 1:
-            grid[key] = 1
+        if chg_log[key] == 1:       grid[key] = 1
         else:
-            if key in grid.keys():
-                grid[key] = 0
+            if key in grid.keys():  grid[key] = 0
 
     return grid
 
 
-for i in range(100):
+for _ in range(100):
     grid = runOneDay(grid)
 
 print()
