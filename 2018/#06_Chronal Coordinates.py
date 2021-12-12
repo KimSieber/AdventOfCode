c405709929dcad37316946625cd494e2f5b07597
Kim <58223175+KimSieber@users.noreply.github.com>
2021-12-12 20:47:56 +0100

    Add files via upload

---
+++ 2018/#06_Chronal Coordinates.py
@@ -0,0 +1,55 @@
+##########################
+### Advent of Code 2018
+###
+### Day 06 - Chronal Coordinates
+###
+### author:  Kim Sieber
+### create:  12.12.2021
+################################################
+
+interferences = [[int(point) for point in line.strip().split(',')] for line in open("#06 Input", "r")]
+
+
+def buildBoards(interferences):
+    sizeX  = max( [ x[0] for x in interferences ] ) + 2
+    sizeY  = max( [ y[1] for y in interferences ] ) + 2 
+    board1 = [ [ None for _ in range(sizeX) ] for _ in range(sizeY) ]
+    board2 = [ [ 0    for _ in range(sizeX) ] for _ in range(sizeY) ]
+    for y in range(sizeY):
+        for x in range(sizeX):
+            distances        = [ abs(interferences[point][0]-x) + abs(interferences[point][1]-y) \
+                                 for point in range(len(interferences))                             ]
+            closest_distance = min(distances)
+            closest_point    = distances.index(closest_distance) if distances.count(closest_distance) == 1 else None
+            board1[y][x]     = closest_point
+            board2[y][x]     = sum(distances)
+    return board1, board2
+
+
+def getFinitePoints(board, interferences):
+    finitive_points = [p for p in range(len(interferences))]
+    maxX = len(board[0]) - 1
+    maxY = len(board   ) - 1
+    for y in range(maxY):
+         if board[y   ][0   ] in finitive_points:  finitive_points.remove( board[y   ][0   ] )
+         if board[y   ][maxX] in finitive_points:  finitive_points.remove( board[y   ][maxX] )
+    for x in range(maxX):
+         if board[0   ][x   ] in finitive_points:  finitive_points.remove( board[0   ][x   ] )
+         if board[maxY][x   ] in finitive_points:  finitive_points.remove( board[maxY][x   ] )
+    return finitive_points
+
+
+board1, board2 = buildBoards( interferences )
+finite_points  = getFinitePoints( board1, interferences )
+largest_area   = max( [ sum( [ sum( [ 1 for p in line if p==point ] ) for line in board1 ] ) for point in finite_points ] )
+        
+print ('Solution Part I   : ', largest_area)
+
+print ('Solutino Part II  : ', sum( [ sum( [ 1 for point in line if point < 10000 ] ) for line in board2 ] ) )
+
+
+
+
+
+
+
